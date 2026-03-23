d939d7d31a7d3f72b7955ec8f509d68c42c3531c
Javi Roman <jroman.espinar@gmail.com>
2026-03-23 16:22:54 +0100

    sync

--- bsp/init-yocto-bsp.sh
+++ bsp/init-yocto-bsp.sh
@@ -10,8 +10,19 @@ if [ -d "bitbake" ]; then
     echo "[INFO] bitbake ya existe en ${WORKSPACE_DIR}/bitbake; se omite clone"
 else
     git clone https://git.openembedded.org/bitbake
-    rm -rf bitbake/.git
 fi
 
 ./bitbake/bin/bitbake-setup init --non-interactive poky-master poky-with-sstate distro/poky machine/qemux86-64
 
+rm -rf bitbake
+
+# Add custom layers to:
+# bitbake-builds/poky-master/build/conf/bblayers.conf
+bitbake-layers add-layer meta-odoo-pos
+bitbake-layers show-layers
+
+echo "source bitbake-builds/poky-master/build/init-build-env"
+# core-image-weston: A very basic Wayland image with a terminal.
+# This image provides the Wayland protocol libraries and the
+# reference Weston compositor.
+echo "bitbake core-image-weston"
